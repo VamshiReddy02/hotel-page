759783c884143c0837905725bdba29d3403f3d5a
VamshiReddy02 <vamshiproject02@gmail.com>
2025-05-28 13:31:59 +0530

    first commit
    Signed-off-by: VamshiReddy02 <vamshiproject02@gmail.com>

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+
+const Navbar = () => {
+  return (
+    <section className='py-6.5'>
+        <div className='px-3 md:px-4 lg:px-10'>
+            <div className='flex justify-between items-center'>
+                <div className='font-Manrope-Bold text-2xl md:text-4xl lg:text-[2.5rem]'>nueve</div>
+                <div className="mt-1">
+                    <svg width="80" height="35" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" className="cursor-pointer">
+                        <rect y="8" width="60" height="1" fill="#1a1a1a"/>
+                        <rect y="18" width="60" height="1" fill="#1a1a1a"/>
+                        <rect y="28" width="60" height="1" fill="#1a1a1a"/>
+                    </svg>
+                </div>
+            </div> 
+        </div>
+    </section>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+       "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+       "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {
+            fontFamily: {
+                "Manrope-Bold": ["Manrope-Bold", "sans-serif"],
+                "Manrope-ExtraBold": ["Manrope-ExtraBold", "sans-serif"],
+                "Manrope-ExtraLight": ["Manrope-ExtraLight", "sans-serif"],
+                "Manrope-Light": ["Manrope-Light", "sans-serif"],
+                "Manrope-Regular": ["Manrope-Regular", "sans-serif"],
+            },
+            backgroundImage: {
+                'custom-gradient': 'linear-gradient(to bottom, #666 0%, #f0f0f0 20%, #999 100%)',
+            },
+        },
+        
+    },
+    plugins: [],
+};
+export default config;
